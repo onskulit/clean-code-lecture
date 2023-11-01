66d91ca514db816d8cc319750af6b379b6548ccf
Dmitriy Latyshev <dislaty@mail.ru>
2023-11-01 16:12:19 +0100

    refactor: add eslint

--- index.js
+++ index.js
@@ -34,38 +34,46 @@ const elementCreator = (elem, classNames, child, parent) => {
 
 const ONE = 1;
 const FIVE = 5;
-const MAN_I_AM_DEAD = true;
 let formIsValid = false;
 
 const validateForm = () => {
     const validate = (type, value) => {
         switch (type) {
-            case 'name':
+            case 'name': {
                 const regexName = /[а-яё]/i;
                 if (!value) {
                     return 'Введите имя';
-                } else if (!regexName.test(value)) {
+                }
+                if (!regexName.test(value)) {
                     return 'Имя должно содержать только буквы русского алфавита';
                 }
                 break;
-            case 'surname':
+            }
+            case 'surname': {
                 const regexSurname = /[а-яё]/i;
                 if (!value) {
                     return 'Введите фамилию';
-                } else if (!regexSurname.test(value)) return `Фамилия должна содержать только буквы русского алфавита`;
+                }
+                if (!regexSurname.test(value)) return `Фамилия должна содержать только буквы русского алфавита`;
 
                 break;
+            }
             case `grade`:
-                if (!value || value > FIVE || value < ONE || !parseInt(value)) {
+                if (!value || value > FIVE || value < ONE || !parseInt(value, 10)) {
                     if (!value) {
                         return 'Введите оценку';
-                    } else if (!parseInt(value)) {
+                    }
+                    if (!parseInt(value, 10)) {
                         return `Оценка должна быть цифрой от 1 до 5`;
-                    } else if (value > 5) return 'Оценка не должна быть больше 5';
-                    else if (value < 1) {
+                    }
+                    if (value > 5) return 'Оценка не должна быть больше 5';
+                    if (value < 1) {
                         return 'Оценка не должна быть меньше 1';
                     }
                 }
+                break;
+            default:
+                return undefined;
         }
     };
 
@@ -78,7 +86,7 @@ const validateForm = () => {
     const gradeContainer = document.getElementById('grade-container');
 
     const containers = [nameContainer, surnameContainer, gradeContainer];
-    for (let i = 0; i < containers.length; i++) {
+    for (let i = 0; i < containers.length; i += 1) {
         const error = containers[i].querySelector('.error');
         if (error) {
             error.remove();
@@ -111,11 +119,9 @@ const validateForm = () => {
 
 // Точка входа в программу
 
-const getList = () => {
-    return document.getElementById('list');
-};
+const getList = () => document.getElementById('list');
 
-document.querySelector('form').addEventListener('submit', function (e) {
+document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault();
 
     validateForm();
@@ -126,11 +132,11 @@ document.querySelector('form').addEventListener('submit', function (e) {
 
     const nameInput = document.querySelector('input[name="name"]');
     const surnameInput = document.querySelector('input[name="surname"]');
-    var gradeInput = document.querySelector('input[name="grade"]');
-    var list = getList();
+    const gradeInput = document.querySelector('input[name="grade"]');
+    const list = getList();
 
     const name = nameInput.value;
-    let surname = surnameInput.value;
+    const surname = surnameInput.value;
     const grade = gradeInput.value;
 
     const isNotGoodGrade = grade < 3;
@@ -141,7 +147,7 @@ document.querySelector('form').addEventListener('submit', function (e) {
         undefined,
         list
     );
-    item.textContent = name + ' ' + surname + ' - ' + grade;
+    item.textContent = `${name} ${surname} - ${grade}`;
 
     nameInput.value = ``;
     surnameInput.value = '';
